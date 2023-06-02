2db2c0ee98c09c75df810356bcaffd74e3791c24
Lxdovic <ludovicdebever0@gmail.com>
2023-06-02 09:29:39 +0200

    fix(ProjectsCard): Fixed width on large screens

--- src/features/Sections/Projects/ProjectCard.tsx
+++ src/features/Sections/Projects/ProjectCard.tsx
@@ -3,7 +3,7 @@ const ProjectCard = (props: any) => {
 
   return (
     <a href={href} className="panel lg:w-[50rem] h-1/2 px-4 md:px-10">
-      <div className="overflow-hidden relative border hover:bg-darkest/60 transition w-screen flex flex-col gap-4 border-white/25 rounded-xl p-10 h-full">
+      <div className="overflow-hidden relative border hover:bg-darkest/60 transition lg:w-auto w-screen flex flex-col gap-4 border-white/25 rounded-xl p-10 h-full">
         <div className="flex gap-4">{tags}</div>
         <h3 className="text-2xl md:text-4xl font-['Poppins-Semibold'] text-white/80">
           {title}
